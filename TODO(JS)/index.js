b963ba9747cc4a3b6725b592cac729e2d2f338b7
github ryuya25 <kagiya20100708@gmail.com>
2024-11-27 15:30:39 +0900

    Chapter37 タスクの追加機能(ボタン) 完了

--- TODO(JS)/index.js
+++ TODO(JS)/index.js
@@ -25,8 +25,25 @@ const onClickAdd = () => {
     p.className = "todo-item"
     p.innerText = inputText;
 
+    // button(完了)生成
+    const completeButton = document.createElement("button")
+    completeButton.innerText = "完了"
+    completeButton.addEventListener("click", () => {
+        alert("完了");
+    })
+
+    // button(削除)生成
+    const deleteButton = document.createElement("button")
+    deleteButton.innerText = "削除"
+    deleteButton.addEventListener("click", () => {
+        alert("削除");
+    })
+
+
     // 階層構造を追加
     div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
     li.appendChild(div);
 
     // 未完了のTODOリスト(imcomplete-list)に対して、上記までで作成したHTMLを追加
