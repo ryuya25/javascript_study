f544c15c1b8d3235ab867530f441e3c96002477c
github ryuya25 <kagiya20100708@gmail.com>
2024-11-27 15:39:39 +0900

    Chapter38  タスク削除機能 完了

--- TODO(JS)/index.js
+++ TODO(JS)/index.js
@@ -36,9 +36,10 @@ const onClickAdd = () => {
     const deleteButton = document.createElement("button")
     deleteButton.innerText = "削除"
     deleteButton.addEventListener("click", () => {
-        alert("削除");
+        // 押された削除ボタンの親にあるliタグを未完了リストから削除
+        const deleteTarget = deleteButton.closest("li");
+        document.getElementById("imcomplete-list").removeChild(deleteTarget);
     })
-
 
     // 階層構造を追加
     div.appendChild(p);
